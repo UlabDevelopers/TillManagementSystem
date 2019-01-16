0fcee8f80d0812847ba5a258b33b8d3b20a9ee2e
shamimaakther <shamima.akther.cse@ulab.edu.bd>
2019-01-17 00:29:24 +0600

    History info1

---
+++ Source/History1.java
@@ -0,0 +1,65 @@
+package canteenmanagementsystem;
+
+import static canteenmanagementsystem.CMS.DRIVER;
+import static canteenmanagementsystem.CMS.JDBC_URL;
+import static canteenmanagementsystem.CMS.con;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class History1
+{
+    private String date;
+    private String cName;
+    private String proName;
+    private int proNumber;
+    private double proCost;
+    private String cType;
+    private String proType;
+    public History1(String Date,String CName,String ProName,int ProNumber,double ProCost,String CType,String ProType)
+    {
+        this.date=Date;
+        this.cName=CName;        
+        this.proName=ProName;
+        this.proNumber=ProNumber;
+        this.proCost=ProCost;
+        this.cType=CType;
+        this.proType=ProType;
+    }
+    public String getDate()
+    {
+        return date;
+    }
+    public String getCName()
+    {
+        return cName;
+    }
+    public String getProName()
+    {
+        return proName;
+    }
+    public int getProNumber()
+    {
+        return proNumber;
+    }
+    public double getProCost()
+    {
+        return proCost;
+    }
+    public String getCType()
+    {
+        return cType;
+    }
+    public String getProType()
+    {
+        return proType;
+    }
+}
+
+
+
+
+
